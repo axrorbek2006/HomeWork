d80f296608e239d946c1795bc336d38500437797
axrorbek2006 <122671790+axrorbek2006@users.noreply.github.com>
2023-06-22 23:10:45 +0500

    Task2

--- Tasks/Tasks/Program.cs
+++ Tasks/Tasks/Program.cs
@@ -4,7 +4,50 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" Salom ");
+            Console.WriteLine(" Salom \n ");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(" Enter number : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(" \n Even nums \n ");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Thread thread1 = new Thread(() => PrintEvenNum(num));
+            Thread thread2 = new Thread(() => PrintOddNum(num));
+
+            thread1.Start();
+
+            thread2.Start();
+
+
+            void PrintEvenNum(int num ) 
+            {
+                for (int i = 1; i <= num; i++) 
+                { 
+                    if (i % 2 == 0)
+                    {
+                        Console.WriteLine(i+"\n");
+                    }
+                }
+
+                
+            }
+
+            void PrintOddNum(int num)
+            {
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(" \n Odd nums \n ");
+                Console.ForegroundColor = ConsoleColor.White;
+                for (int i = 1; i <= num; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        Console.WriteLine(i + "\n");
+                    }
+                }
+            }
         }
     }
 }
