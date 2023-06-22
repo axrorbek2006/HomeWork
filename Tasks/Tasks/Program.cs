868810600c46522f803fcdaf8dd954173a2f3a0d
axrorbek2006 <122671790+axrorbek2006@users.noreply.github.com>
2023-06-22 22:48:42 +0500

    Task1

--- Tasks/Tasks/Program.cs
+++ Tasks/Tasks/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace Tasks
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Tasks
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine(" Salom ");
+            Console.ForegroundColor= ConsoleColor.White;
+
+            Thread thread = new Thread(() => PrintNum(23));
+
+            thread.Start();
+            
+            void PrintNum(int Num)
+            {
+                for (int i = 1; i <= Num; i++) 
+                {
+                    Console.WriteLine($"Thread Name : {i} \n ");
+                }
+            }
         }
     }
 }
